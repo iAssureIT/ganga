dbeafec15e78af1020b4fadb71b5892d182997ee
priyanka-kale <kalepriya100@gmail.com>
2019-09-18 10:32:43 +0530

    Priyanka kale added footer2 and footer3

--- gangacustomer/src/common/Footer/Footer.js
+++ gangacustomer/src/common/Footer/Footer.js
@@ -4,14 +4,32 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-
+import axios from 'axios';
 import './Footer.css';
 
 export default class Footer extends Component {
 
-
-  render(){
+    constructor(props){
+    super(props);
+        this.state = {
+          categoryDetails:[]
+        }
+    }
+    componentDidMount(){
+     
+      axios.get("/api/category/get/list")
+                .then((response)=>{
+                  this.setState({
+                      categoryDetails : response.data
+                  })
+                })
+                .catch((error)=>{
+                    console.log('error', error);
+                })  
+    }
+    render(){
        return(
+        <div >
         <div className="container">
             <div className="footer col-lg-12 col-md-12 col-xs-12 col-sm-12">
                 <div className="footer-middle">
@@ -70,51 +88,51 @@ export default class Footer extends Component {
                 <br />
                 <br />
                 <div className="categoryDiv row">
-                    <div className="col-md-3 col-lg-3 col-sm-3 col-xs-6">
-                        <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
-                            <strong>MOBILES & TABLETS</strong><br/><br/> 
-                        </div>
-                        <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
-                            <p>
-                                Nokia, Samsung, Sony, Blackberry, Asus, HTC, Oppo, Lenovo, Alcatel, iPhone 4s, iPhone 5s, iPhone 6, Apple iPhone 6s, iPad, Samsung Tablet, Acer Tablet, Lenovo Tablet...
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-lg-3 col-sm-3 col-xs-6">
-                        <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
-                            <strong>TELEVISIONS</strong><br/><br/> 
-                        </div>
-                        <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
-                            <p>
-                                Sony TV, LG TV, Toshiba TV, Samsung TV, Panasonic TV, Sharp TV, LCD TV, Samsung LCD TV, LG LCD TV, Sharp LCD TV, LED TV, Sony LED TV, Samsung LED TV...
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-lg-3 col-sm-3 col-xs-6">
-                        <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
-                            <strong>HEALTH & BEAUTY</strong><br/><br/> 
-                        </div>
-                        <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
-                            <p>
-                                Bourjois, L'oreal, The Body Shop, Maybeline, Shiseido...
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-md-3 col-lg-3 col-sm-3 col-xs-6">
-                        <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
-                            <strong>AIR CONDITIONERS</strong><br/><br/> 
-                        </div>
-                        <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
-                            <p>
-                               Daikin, Electrolux, LG, Mitsubishi, Panasonic, Samsung, Sharp, Toshiba, Gree...
-                            </p>
-                        </div>
-                    </div>
+                    {
+                       
+                        this.state.categoryDetails && this.state.categoryDetails.map((data,index)=>{
+                            var subCategoryStr = '';
+                            if (data.subCategory) {
+                                subCategoryStr = data.subCategory.map( (subcat) =>{
+                                    return  subcat['subCategoryTitle'] ;
+                                  }).join(', ');
+                            }
+                              
+                            return(
+                                <div className="col-md-3 col-lg-3 col-sm-3 col-xs-6">
+                                    <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
+                                        <strong>{data.category}</strong><br/><br/> 
+                                    </div>
+                                    <div className="col-md-12 col-lg-12 col-sm-12 col-xs-12 NoPadding">
+                                        <p>
+                                        {subCategoryStr}
+                                        </p>
+                                    </div>
+                                </div>
+                                );
+                            
+                        })
+                    }
+                    
                 </div>
                 <br />
             </div>
-
-        </div>    
+        </div> 
+        <div className="row footer3">
+            <div className="container">
+            <div className="footer_bottom">
+                <div className="col-sm-9 col-md-7 col-lg-6">
+                    <p>GangaExpress Copyright <i className="fa fa-copyright"></i> 2019 - 2020. All Rights Reserved.</p>
+                </div>
+                <div className="col-sm-3 col-md-5 col-lg-6">
+                <a href="#">
+                    <img src="http://demo8.cmsmart.net/mag2_amazon_themeforest/pub/media/multistore/icon/icon-footer.png" alt="" />
+                </a>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>   
         );
   } 
 
