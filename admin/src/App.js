87c2c8341983f232945526bf9c5f613494f1dae6
Miss. Kharare <khararemanisha555@gmail.com>
2019-10-11 11:55:32 +0530

    Manisha added changes

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,7 +5,7 @@ import Layout from './coreAdmin/Layout/Layout.js';
 import './App.css';
 
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3060';
+axios.defaults.baseURL = 'http://gangaapi.nurseryworld.in';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 function App() {
