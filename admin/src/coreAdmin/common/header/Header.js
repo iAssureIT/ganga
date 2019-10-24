40754fc559e3c0e39e5d0dbe420a1d8d6c95f90c
Miss. Kharare <khararemanisha555@gmail.com>
2019-10-24 15:38:13 +0530

    Manisha added chnages

--- admin/src/coreAdmin/common/header/Header.js
+++ admin/src/coreAdmin/common/header/Header.js
@@ -75,7 +75,7 @@ logout(){
                   
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-1 col-xs-1 pull-right text-right">
-                  &nbsp;{ localStorage.getItem('userName') != 'undefined' ?  localStorage.getItem('userName') : ""}
+                  { localStorage.getItem('userName') != 'undefined' ?  localStorage.getItem('userName') : ""}
                   &nbsp; &nbsp;
                     <i className="fa fa-user  headicon "></i>
                 </div>
